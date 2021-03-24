8942a1a02fa81834b6df52f5055f8b9f5a4cca57
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-24 15:50:08 -0300

    feat: read and print content of a file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Arquivo index');
+// File System para manipular arquivos
+const fs = require('fs');
+
+// Função para leitura de arquivos, utilizando callback para receber erro e dados
+// Encoding para informar ao node que esse é um arquivo de texto
+fs.readFile('./arquivo.txt', { encoding: 'utf-8' }, (error, data) => {
+  if (error) {
+    console.log('Ocorreu um erro durante a leitura');
+  } else {
+    console.log(data);
+  }
+});
